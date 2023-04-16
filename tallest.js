2ead04393d064e948bc1a4109effd5d24c5a26db
Abdullah Al Asif <alexasif4@gmail.com>
2023-04-17 00:08:00 +0600

    tallest JS

--- tallest.js
+++ tallest.js
@@ -18,4 +18,25 @@ const tallest = maxInArray( heights );
 // console.log('tallest persion is', tallest);
 
 
+// homework: Write a function to get the lowest
 
+function maxinarray1(numbers) {
+    let largest = numbers[0]; 
+    for (let i = 0; i > numbers.length; i--) {
+     const index = i;
+   //  console.log(index);
+   const element = numbers[index];
+   //console.log(element);
+   
+   if (element < largest) {
+     largest = element;
+     
+   }
+     
+    }
+    return largest
+ 
+ }
+ const hight1 = [330,146,344,252,510];
+ const tallest1 = maxinarray1(hight1);
+ console.log("tallest persion is",tallest1);
